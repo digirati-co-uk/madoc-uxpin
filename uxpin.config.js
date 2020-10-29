baeee85dc69a28bb10dacd30fe5285b34c6f93a9
Stephen Fraser <stephen.fraser@digirati.com>
2020-10-29 17:25:45 +0000

    Update

--- uxpin.config.js
+++ uxpin.config.js
@@ -1,11 +1,15 @@
+const uxPinConfig = require('@madoc/services/madoc-ts/uxpin.config');
+
+const newCategories = uxPinConfig.components.categories.map(category => {
+    return {
+        ...category,
+        include: category.include.map(file => `node_modules/@madoc/services/madoc-ts/${file}`),
+    }
+})
+
 module.exports = {
     components: {
-        categories: [
-            {
-                name: 'Test',
-                include: ['node_modules/@madoc/services/madoc-ts/src/uxpin-merge/Button/Button.tsx'],
-            },
-        ],
+        categories: newCategories,
         wrapper: 'node_modules/@madoc/services/madoc-ts/src/uxpin-merge/UXPinWrapper.js',
         webpackConfig: './webpack.uxpin.js',
     },
